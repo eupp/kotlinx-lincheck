3a80264afd55d653f9ccb7142c40f4145685ffbd
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2023-03-17 12:50:35 +0100

    Runner WIP: run init/post part in separate threads
    Signed-off-by: Evgenii Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/runner/TestThreadExecution.java
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/runner/TestThreadExecution.java
@@ -15,7 +15,7 @@ import static org.jetbrains.kotlinx.lincheck.UtilsKt.*;
 
 
 /**
- * Instance of this class represents the test execution for ONE thread. Several instances should be ran in parallel.
+ * Instance of this class represents the test execution for ONE thread. Several instances should be run in parallel.
  * All implementations of this class should be generated via {@link TestThreadExecutionGenerator}.
  * The results and clocks are in the `results`  and `clocks` fields correspondingly.
  *
@@ -30,6 +30,8 @@ public abstract class TestThreadExecution implements Runnable {
     public TestThreadExecution[] allThreadExecutions;
 
     public Result[] results; // for ExecutionResult
+
+    public int iThread; // thread ID of this execution
     public int[][] clocks; // for HBClock
     public volatile int curClock;
     public boolean useClocks;
