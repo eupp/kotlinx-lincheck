1ce67d23958bbc7313aac67c126e522802d78aae
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-08-07 11:17:05 +0200

    fix bug in MethodCallTransformer and repair old model checking strategy
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/transformers/MethodCallTransformer.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/transformers/MethodCallTransformer.kt
@@ -72,11 +72,9 @@ internal class MethodCallTransformer(
         // STACK [INVOKEVIRTUAL]: owner, owner, className, methodName, codeLocation, methodId
         // STACK [INVOKESTATIC]:         null, className, methodName, codeLocation, methodId
         pushArray(argumentLocals)
-        // STACK: ..., argumentsArray
+        // STACK: ..., arguments
         invokeStatic(Injections::beforeMethodCall)
         invokeBeforeEventIfPluginEnabled("method call $methodName", setMethodEventId = true)
-        // STACK [INVOKEVIRTUAL]: owner, arguments
-        // STACK [INVOKESTATIC] :        arguments
         val methodCallEndLabel = newLabel()
         val handlerExceptionStartLabel = newLabel()
         visitTryCatchBlock(methodCallStartLabel, methodCallEndLabel, handlerExceptionStartLabel, null)
@@ -104,10 +102,10 @@ internal class MethodCallTransformer(
                 }
             )
         } else {
+            // STACK: shouldInterceptMethodResult
+            pop()
             loadLocals(argumentLocals)
             visitMethodInsn(opcode, owner, name, desc, itf)
-            // STACK: shouldInterceptMethodCallResult
-            pop()
         }
         visitLabel(methodCallEndLabel)
         // STACK [INVOKEVIRTUAL]: owner, arguments
