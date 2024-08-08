8acce329368bee77ce29bd1af01faf0deb666bb1
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-08-09 00:10:41 +0200

    fix a BATSHIT CRAZY BUG in the coroutines runner
    * So, apparently, the `context` used in the `ParallelThreadsRunner::Completion`
      can somehow escape between different invocations of the test.
      If this happens, everything becomes really FUCKED UP, and I spent hours trying to debug it.
      It leads to a situation, when on the new run of the test, an old `ParallelThreadRunnerInterceptor` object
      (stored in the `context`) from the previous invocation is used instead.
      To fix the issue, I changed the `Completion::reset` logic.
      Instead of creating new interceptor object, I re-use the same object by reset its state.
      Since we only use completions in a very specific way in `ParallelThreadsRunner`
      (basically one separate completion object for each suspending actor)
      this should not, in theory, lead to any problem.
      Still, only Gods know if this is truly correct way to fix this.
    
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/runner/ParallelThreadsRunner.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/runner/ParallelThreadsRunner.kt
@@ -101,7 +101,9 @@ internal open class ParallelThreadsRunner(
     protected inner class Completion(private val iThread: Int, private val actorId: Int) : Continuation<Any?> {
         val resWithCont = SuspensionPointResultWithContinuation(null)
 
-        override var context = ParallelThreadRunnerInterceptor(resWithCont) + StoreExceptionHandler() + Job()
+        private val interceptor = ParallelThreadRunnerInterceptor(resWithCont)
+
+        override var context = interceptor + StoreExceptionHandler() + Job()
 
         // We need to run this code in an ignored section,
         // as it is called in the testing code but should not be analyzed.
@@ -124,7 +126,8 @@ internal open class ParallelThreadsRunner(
 
         fun reset() {
             resWithCont.set(null)
-            context = ParallelThreadRunnerInterceptor(resWithCont) + StoreExceptionHandler() + Job()
+            interceptor.reset(resWithCont)
+            context = interceptor + StoreExceptionHandler() + Job()
         }
 
         /**
@@ -137,9 +140,12 @@ internal open class ParallelThreadsRunner(
             private var resWithCont: SuspensionPointResultWithContinuation
         ) : AbstractCoroutineContextElement(ContinuationInterceptor), ContinuationInterceptor {
 
+            var continuation: Continuation<Any?>? = null
+
             // We need to run this code in an ignored section,
             // as it is called in the testing code but should not be analyzed.
             override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> = runInIgnoredSection {
+                this.continuation = (continuation as Continuation<Any?>)
                 return Continuation(StoreExceptionHandler() + Job()) { result ->
                     runInIgnoredSection {
                         // decrement completed or suspended threads only if the operation was not cancelled
@@ -150,11 +156,16 @@ internal open class ParallelThreadsRunner(
                                 completedOrSuspendedThreads.incrementAndGet()
                             }
                             @Suppress("UNCHECKED_CAST")
-                            resWithCont.set(result to continuation as Continuation<Any?>)
+                            resWithCont.set(result to this.continuation as Continuation<Any?>)
                             onResumeCoroutine(iThread, actorId)
                         }
                     }
                 }
+            }
+
+            fun reset(resWithCont: SuspensionPointResultWithContinuation) {
+                this.resWithCont = resWithCont
+                this.continuation = null
             }
         }
     }
