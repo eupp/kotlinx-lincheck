7d70403b6a17e6c3fd71ce70c36df9572a879b66
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-12-20 23:10:34 +0100

    improve `ObjectTracker` docs
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ObjectTracker.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ObjectTracker.kt
@@ -15,18 +15,49 @@ import java.lang.ref.WeakReference
 import kotlin.coroutines.Continuation
 
 /**
- * Tracks object allocations and changes in object graph topology.
+ * Tracks objects and changes in object graph topology.
+ *
+ * The object tracker allows registering objects within the managed strategy,
+ * assigning to each object a unique serial number and unique id.
+ *
+ * The unique serial number is a 32-bit integer number.
+ * Assuming the deterministic execution, the serial numbers are guaranteed to be generated in the same order,
+ * providing a persistent objects numeration between different re-runs of the same program execution.
+ *
+ * The unique object id is a 64-bit integer number, constructed from
+ * the object's serial number and its identity hash code.
+ *
+ * The registered objects are associated with the registry entries [ObjectEntry],
+ * keeping object's serial number, its identity hash code, a weak reference to the object,
+ * and, potentially, other meta-data (defined by the concrete implementations of the interface).
+ * The tracker allows retrieving the registry entry either by the object reference itself
+ * or by unique object id.
  */
 interface ObjectTracker {
 
+    /**
+     * Retrieves the registry entry associated with the given object id.
+     *
+     * @param id the object id to retrieve the corresponding entry for.
+     * @return the corresponding [ObjectEntry],
+     *   or null if no entry is associated with the given object id.
+     */
     operator fun get(id: ObjectID): ObjectEntry?
 
+    /**
+     * Retrieves the registry entry associated with the given object.
+     *
+     * @param obj the object to retrieve the corresponding entry for.
+     * @return the corresponding [ObjectEntry],
+     *   or null if no entry is associated with the given object.
+     */
     operator fun get(obj: Any): ObjectEntry?
 
     /**
      * Registers a newly created object in the object tracker.
      *
-     * @param obj the object to be registered
+     * @param obj the object to be registered.
+     * @return the registry entry for the new object.
      */
     fun registerNewObject(obj: Any): ObjectEntry
 
@@ -36,7 +67,7 @@ interface ObjectTracker {
      * but that nonetheless leaked into the analyzed code and thus needs to be tracked as well.
      *
      * @param obj the external object to be registered.
-     * @return the corresponding [ObjectEntry] which represents the registered object entry in the tracker.
+     * @return the registered object entry for the external object.
      */
     fun registerExternalObject(obj: Any): ObjectEntry
 
@@ -58,36 +89,87 @@ interface ObjectTracker {
      */
     fun shouldTrackObjectAccess(obj: Any): Boolean
 
+    /**
+     * Retains only the entries in the object tracker that match the given predicate.
+     *
+     * @param predicate a condition that determines which entries should be retained.
+     */
     fun retain(predicate: (ObjectEntry) -> Boolean)
 
     /**
-     * Resets the state of the object tracker.
+     * Resets the state of the object tracker, removing all registered object entries.
      */
     fun reset()
 }
 
+/**
+ * Registers an object in the tracker if it is not already present.
+ * If the object is already registered, returns its corresponding entry.
+ *
+ * @param obj the object to be registered if absent.
+ * @return the registered entry associated with the given object.
+ */
 fun ObjectTracker.registerObjectIfAbsent(obj: Any): ObjectEntry =
     get(obj) ?: registerExternalObject(obj)
 
 
+/**
+ * Represents an entry for the tracked object.
+ *
+ * @property objNumber A unique serial number for the object.
+ * @property objHashCode The identity hash code of the object.
+ * @property objReference A weak reference to the associated object.
+ */
 open class ObjectEntry(
     val objNumber: Int,
     val objHashCode: Int,
     val objReference: WeakReference<Any>,
 )
 
+/**
+ * A unique identifier of an object.
+ *
+ * The identifier is a 64-bit integer number, formed by combining its serial number and hash code.
+ * The serial number [ObjectEntry.objNumber] is stored in the higher 32-bits of the id, while
+ * the identity hash code [ObjectEntry.objHashCode] is stored in the lower 32-bits.
+ */
 val ObjectEntry.objId: ObjectID get() =
     (objNumber.toLong() shl 32) + objHashCode.toLong()
 
+/**
+ * Extracts and returns the object number from the given object id.
+ */
 fun ObjectID.getObjectNumber(): Int =
     (this ushr 32).toInt()
 
+/**
+ * Extracts and returns the identity hash code of the object from the given object id.
+ *
+ * @return The integer hash code derived from the ObjectID.
+ */
 fun ObjectID.getObjectHashCode(): Int =
     this.toInt()
 
+/**
+ * Retrieves the unique serial object number for the given object.
+ *
+ * @param obj the object for which the object number is to be retrieved.
+ * @return the unique object number if the object is registered in the tracker,
+ *   or -1 if no entry is associated with the given object.
+ */
 fun ObjectTracker.getObjectNumber(obj: Any): Int =
     get(obj)?.objNumber ?: -1
 
+/**
+ * Generates a string representation of an object.
+ *
+ * Provides an adorned string representation for null values, primitive types, strings, enums,
+ * and some other types of objects.
+ * All other objects are represented in the format `className#objectNumber`.
+ *
+ * @param obj the object to generate a textual representation for.
+ * @return a string representation of the specified object.
+ */
 fun ObjectTracker.getObjectRepresentation(obj: Any?) = when {
     // null is displayed as is
     obj == null -> "null"
@@ -140,6 +222,12 @@ private fun objectClassNameRepresentation(obj: Any): String = when (obj) {
     else            -> obj.javaClass.simpleName
 }
 
+/**
+ * Abstract base class for tracking objects.
+ *
+ * It provides an implementation for registering, retrieving, updating,
+ * and managing objects and their entries in the registry.
+ */
 abstract class AbstractObjectTracker : ObjectTracker {
 
     // counter of all registered objects
@@ -176,8 +264,20 @@ abstract class AbstractObjectTracker : ObjectTracker {
         return entry
     }
 
+    /**
+     * Represents the kind of object being tracked.
+     *
+     * Can be either:
+     *   - [NEW] - Newly created object registered in the tracker.
+     *   - [EXTERNAL] - External object registered in the tracker.
+     */
     protected enum class ObjectKind { NEW, EXTERNAL }
 
+    /**
+     * Method responsible for creating an instance of [ObjectEntry] for tracking an object in the system.
+     * Derived classes may override this method to create their own customized instances of [ObjectEntry]
+     * with additional meta-data.
+     */
     protected open fun createObjectEntry(
         objNumber: Int,
         objHashCode: Int,
@@ -215,7 +315,7 @@ abstract class AbstractObjectTracker : ObjectTracker {
         objectIndexCapacity = INITIAL_OBJECT_INDEX_CAPACITY
     }
 
-    /**
+    /*
      * Performs garbage collection for the object registry by
      * removing from the index entries that are associated with garbage-collected objects.
      */
@@ -232,7 +332,7 @@ abstract class AbstractObjectTracker : ObjectTracker {
         }
     }
 
-    /**
+    /*
      * Cleans up the current list of `ObjectEntry` instances
      * by removing entries that reference garbage-collected objects.
      */
