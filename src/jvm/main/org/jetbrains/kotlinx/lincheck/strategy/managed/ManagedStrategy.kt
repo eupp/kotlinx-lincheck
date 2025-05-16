80e533a3a365e1f69db4c04a42c80e4cf28a8bcd
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-05-16 02:15:49 +0200

    minor refactoring of `onNextExecutionPoint`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ManagedStrategy.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ManagedStrategy.kt
@@ -482,9 +482,7 @@ abstract class ManagedStrategy(
             }
             return
         }
-        if (!loopDetector.replayModeEnabled) {
-            loopDetector.onNextExecutionPoint(codeLocation)
-        }
+        loopDetector.onNextExecutionPoint(codeLocation)
     }
 
     private fun processLoopDetectorDecision(
