5a77aa8bdc309b9386c0bf3529b9358c91ca1517
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-05-29 19:35:11 +0200

    group `Options` and `CTestConfiguration` classes in `Configuration.kt`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/Configuration.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/Configuration.kt
@@ -1,21 +1,31 @@
 /*
  * Lincheck
  *
- * Copyright (C) 2019 - 2023 JetBrains s.r.o.
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
  *
  * This Source Code Form is subject to the terms of the
  * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
+
 package org.jetbrains.kotlinx.lincheck
 
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
-import org.jetbrains.kotlinx.lincheck.execution.*
+import org.jetbrains.kotlinx.lincheck.execution.ExecutionGenerator
+import org.jetbrains.kotlinx.lincheck.execution.ExecutionScenario
+import org.jetbrains.kotlinx.lincheck.execution.RandomExecutionGenerator
+import org.jetbrains.kotlinx.lincheck.strategy.Strategy
+import org.jetbrains.kotlinx.lincheck.strategy.managed.ManagedCTestConfiguration
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingCTest
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingCTestConfiguration
+import org.jetbrains.kotlinx.lincheck.strategy.stress.StressCTest
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressCTestConfiguration
-import org.jetbrains.kotlinx.lincheck.strategy.stress.StressOptions
-import org.jetbrains.kotlinx.lincheck.verifier.*
-import org.jetbrains.kotlinx.lincheck.util.DEFAULT_LOG_LEVEL
+import org.jetbrains.kotlinx.lincheck.transformation.InstrumentationMode
 import org.jetbrains.kotlinx.lincheck.util.LoggingLevel
+import org.jetbrains.kotlinx.lincheck.util.DEFAULT_LOG_LEVEL
+import org.jetbrains.kotlinx.lincheck.verifier.Verifier
+import org.jetbrains.kotlinx.lincheck.verifier.linearizability.LinearizabilityVerifier
+import java.lang.reflect.Method
 
 /**
  * Abstract class for test options.
@@ -188,4 +198,106 @@ abstract class Options<OPT : Options<OPT, CTEST>, CTEST : CTestConfiguration> {
             block()
         } as OPT
     }
+}
+
+/**
+ * Abstract configuration for different lincheck modes.
+ */
+abstract class CTestConfiguration(
+    val testClass: Class<*>,
+    val iterations: Int,
+    val invocationsPerIteration: Int,
+    val threads: Int,
+    val actorsPerThread: Int,
+    val actorsBefore: Int,
+    val actorsAfter: Int,
+    val generatorClass: Class<out ExecutionGenerator>,
+    val verifierClass: Class<out Verifier>,
+    val minimizeFailedScenario: Boolean,
+    val sequentialSpecification: Class<*>,
+    val timeoutMs: Long,
+    val customScenarios: List<ExecutionScenario>
+) {
+
+    /**
+     * Specifies the transformation required for this strategy.
+     */
+    internal abstract val instrumentationMode: InstrumentationMode
+
+    abstract fun createStrategy(
+        testClass: Class<*>,
+        scenario: ExecutionScenario,
+        validationFunction: Actor?,
+        stateRepresentationMethod: Method?,
+    ): Strategy
+
+    companion object {
+        const val DEFAULT_ITERATIONS = 100
+        const val DEFAULT_INVOCATIONS = 10_000
+
+        const val DEFAULT_THREADS = 2
+        const val DEFAULT_ACTORS_PER_THREAD = 5
+        const val DEFAULT_ACTORS_BEFORE = 5
+        const val DEFAULT_ACTORS_AFTER = 5
+
+        const val DEFAULT_MINIMIZE_ERROR = true
+        const val DEFAULT_TIMEOUT_MS: Long = 30_000 // 30 sec
+
+        val DEFAULT_EXECUTION_GENERATOR: Class<out ExecutionGenerator?> = RandomExecutionGenerator::class.java
+        val DEFAULT_VERIFIER: Class<out Verifier> = LinearizabilityVerifier::class.java
+    }
+}
+
+internal fun CTestConfiguration.createVerifier() =
+    verifierClass.getConstructor(Class::class.java).newInstance(sequentialSpecification)
+
+internal fun createFromTestClassAnnotations(testClass: Class<*>): List<CTestConfiguration> {
+
+    val stressConfigurations: List<CTestConfiguration> = testClass.getAnnotationsByType(StressCTest::class.java)
+        .map { ann: StressCTest ->
+            StressCTestConfiguration(
+                testClass = testClass,
+                iterations = ann.iterations,
+                threads = ann.threads,
+                actorsPerThread = ann.actorsPerThread,
+                actorsBefore = ann.actorsBefore,
+                actorsAfter = ann.actorsAfter,
+                generatorClass = ann.generator.java,
+                verifierClass = ann.verifier.java,
+                invocationsPerIteration = ann.invocationsPerIteration,
+                minimizeFailedScenario = ann.minimizeFailedScenario,
+                sequentialSpecification = chooseSequentialSpecification(ann.sequentialSpecification.java, testClass),
+                timeoutMs = CTestConfiguration.DEFAULT_TIMEOUT_MS,
+                customScenarios = emptyList()
+            )
+        }
+
+    val modelCheckingConfigurations: List<CTestConfiguration> =
+        testClass.getAnnotationsByType(ModelCheckingCTest::class.java)
+            .map { ann: ModelCheckingCTest ->
+                ModelCheckingCTestConfiguration(
+                    testClass = testClass,
+                    iterations = ann.iterations,
+                    threads = ann.threads,
+                    actorsPerThread = ann.actorsPerThread,
+                    actorsBefore = ann.actorsBefore,
+                    actorsAfter = ann.actorsAfter,
+                    generatorClass = ann.generator.java,
+                    verifierClass = ann.verifier.java,
+                    checkObstructionFreedom = ann.checkObstructionFreedom,
+                    hangingDetectionThreshold = ann.hangingDetectionThreshold,
+                    invocationsPerIteration = ann.invocationsPerIteration,
+                    guarantees = ManagedCTestConfiguration.DEFAULT_GUARANTEES,
+                    minimizeFailedScenario = ann.minimizeFailedScenario,
+                    sequentialSpecification = chooseSequentialSpecification(
+                        ann.sequentialSpecification.java,
+                        testClass
+                    ),
+                    timeoutMs = CTestConfiguration.DEFAULT_TIMEOUT_MS,
+                    customScenarios = emptyList(),
+                    stdLibAnalysisEnabled = false,
+                )
+            }
+
+    return stressConfigurations + modelCheckingConfigurations
 }
