821872eb58151506837daad9943ce155f1af4373
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-09-04 13:28:28 +0200

    WIP: fixes after rebase
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/LinChecker.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/LinChecker.kt
@@ -11,12 +11,11 @@ package org.jetbrains.kotlinx.lincheck
 
 import org.jetbrains.kotlinx.lincheck.execution.*
 import org.jetbrains.kotlinx.lincheck.strategy.*
-import org.jetbrains.kotlinx.lincheck.transformation.*
+import org.jetbrains.kotlinx.lincheck.strategy.managed.ManagedStrategy
 import org.jetbrains.lincheck.*
 import org.jetbrains.lincheck.datastructures.*
 import org.jetbrains.lincheck.datastructures.verifier.*
 import org.jetbrains.lincheck.util.*
-import kotlin.concurrent.*
 import kotlin.reflect.*
 
 /**
@@ -132,11 +131,7 @@ constructor(private val testClass: Class<*>, options: Options<*, *>?) {
         if (ideaPluginEnabled && this is ManagedCTestConfiguration) {
             enableReplayModeForIdeaPlugin()
             runPluginReplay(
-                settings = this.createSettings(),
-                testClass = testClass,
-                scenario = failure.scenario,
-                validationFunction = testStructure.validationFunction,
-                stateRepresentationMethod = testStructure.stateRepresentation,
+                replayStrategy = createStrategy(failure.scenario) as ManagedStrategy,
                 invocations = invocationsPerIteration,
                 verifier = verifier
             )
