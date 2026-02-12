a02e0299fa778cac5d91b0f7bcb84c231fc2d624
Wiliam Dokov <viliam.dokov@gmail.com>
2026-02-12 15:11:48 +0100

    Make `EventStructureObjectTracker` override createObjectEntry with objectNumber, and simplify

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ManagedStrategy.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ManagedStrategy.kt
@@ -1346,7 +1346,10 @@ internal abstract class ManagedStrategy(
                 isFinal = fieldDescriptor.isFinal,
             )
             memoryTracker!!.beforeRead(threadId, codeLocation, location)
+            println("Interceptin memory for $location")
+            Exception().printStackTrace()
             resultInterceptor?.interceptResult(memoryTracker!!.interceptReadResult(threadId))
+            println("Intercepted result ${resultInterceptor?.isResultIntercepted} ${resultInterceptor?.interceptedResult}")
         }
         loopDetector.beforeReadField(obj)
         return
@@ -1778,6 +1781,7 @@ internal abstract class ManagedStrategy(
         params: Array<Any?>,
         interceptor: ResultInterceptor?,
     ): Unit = threadDescriptor.runInsideIgnoredSection {
+        println("ON METHOD CALL")
         val methodDescriptor = context.getMethodDescriptor(methodId)
         // check if the called method is an atomics API method
         // (e.g., Atomic classes, AFU, VarHandle memory access API, etc.)
@@ -1785,6 +1789,8 @@ internal abstract class ManagedStrategy(
         // process method effect on the static memory snapshot
         processMethodEffectOnStaticSnapshot(receiver, params, atomicMethodDescriptor)
         val threadId = threadScheduler.getCurrentThreadId()
+        println("ON METHOD CALL $threadId ${methodDescriptor.methodName} $atomicMethodDescriptor $receiver")
+
         // re-throw abort error if the thread was aborted
         if (threadScheduler.isAborted(threadId)) {
             threadScheduler.abortCurrentThread()
@@ -1807,6 +1813,7 @@ internal abstract class ManagedStrategy(
                 params,
                 atomicMethodDescriptor,
             )
+            println("Intercept!!!!")
             shouldInterceptAtomicMethod = memoryTracker!!.trackAtomicMethodMemoryAccess(
                 receiver,
                 codeLocation,
